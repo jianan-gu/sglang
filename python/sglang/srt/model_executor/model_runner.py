2157470be773265d72badd39d678d54f2b5df73b
jianan-gu <jianan.gu@intel.com>
2025-06-27 15:04:59 +0800

    [feature] Add Int4 cpu W8A4 kernels (awq format) (#35)

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -348,14 +348,14 @@ class ModelRunner:
 
         if not self.is_draft_worker:
             if self.device == "cpu":
-                # Bind OpenMP threads to CPU cores
-                if self.local_omp_cpuid != "all":
-                    torch.ops._C_utils.init_cpu_threads_env(self.local_omp_cpuid)
-
                 # Initialization of shm all_reduce
                 import sgl_kernel.common_ops
-
                 shm_comm_op = sgl_kernel.common_ops
+                # Bind OpenMP threads to CPU cores
+                if self.local_omp_cpuid != "all":
+                    shm_comm_op.init_cpu_threads_env(self.local_omp_cpuid)
+
+
                 # Set local size to hint SGLang to use shared memory based AllReduce
                 os.environ["LOCAL_SIZE"] = str(self.tp_size)
                 shm_comm_op.initialize(self.tp_size, self.tp_rank)
