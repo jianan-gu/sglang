94eaba81b97b059fbe76db40b859f309d906ec8f
blzheng <beilei.zheng@intel.com>
2025-06-30 11:53:36 +0800

    mllama: fix the KV cache size mismatch issue in cross-attn (#29)

--- python/sglang/srt/layers/attention/torch_native_backend.py
+++ python/sglang/srt/layers/attention/torch_native_backend.py
@@ -7,17 +7,28 @@ from torch.nn.functional import scaled_dot_product_attention
 
 from sglang.srt.layers.attention.base_attn_backend import AttentionBackend
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
+from sglang.srt.speculative.eagle_utils import EagleDraftInput, EagleVerifyInput
+from sglang.srt.layers.attention.utils import create_torch_native_kv_indices
 
 if TYPE_CHECKING:
     from sglang.srt.layers.radix_attention import RadixAttention
     from sglang.srt.model_executor.model_runner import ModelRunner
-
 
 class TorchNativeAttnBackend(AttentionBackend):
     def __init__(self, model_runner: ModelRunner):
         super().__init__()
         self.forward_metadata = None
         self.device = model_runner.device
+        max_bs = model_runner.req_to_token_pool.size
+        self.req_to_token = model_runner.req_to_token_pool.req_to_token.clone()
+        self.kv_indptr = [
+            torch.zeros(
+                (max_bs + 1,), dtype=torch.int32, device=model_runner.device
+            ), # encoder
+            torch.zeros(
+                (max_bs + 1,), dtype=torch.int32, device=model_runner.device
+            ), # decoder
+        ]
 
     def init_forward_metadata(self, forward_batch: ForwardBatch):
         """Init the metadata for a forward pass."""
@@ -37,6 +48,7 @@ class TorchNativeAttnBackend(AttentionBackend):
         scaling=None,
         enable_gqa=False,
         causal=False,
+        kv_indices=None,
     ):
         """Run the extend forward by using torch native sdpa op.
 
@@ -87,8 +99,11 @@ class TorchNativeAttnBackend(AttentionBackend):
 
             # get key and value from cache. per_req_tokens contains the kv cache
             # index for each token in the sequence.
-            req_pool_idx = req_pool_indices[seq_idx]
-            per_req_tokens = req_to_token[req_pool_idx, :seq_len_kv]
+            if kv_indices is not None:
+                per_req_tokens = kv_indices
+            else:
+                req_pool_idx = req_pool_indices[seq_idx]
+                per_req_tokens = req_to_token[req_pool_idx, :seq_len_kv]
             per_req_key = k_cache[per_req_tokens].movedim(0, query.dim() - 2)
             per_req_value = v_cache[per_req_tokens].movedim(0, query.dim() - 2)
 
@@ -120,6 +135,7 @@ class TorchNativeAttnBackend(AttentionBackend):
         scaling=None,
         enable_gqa=False,
         causal=False,
+        kv_indices=None,
     ):
         """Run the decode forward by using torch native sdpa op.
 
@@ -156,11 +172,13 @@ class TorchNativeAttnBackend(AttentionBackend):
 
             # get key and value from cache. per_req_tokens contains the kv cache
             # index for each token in the sequence.
-            req_pool_idx = req_pool_indices[seq_idx]
-            per_req_tokens = req_to_token[req_pool_idx, :seq_len_kv]
+            if kv_indices is not None:
+                per_req_tokens = kv_indices
+            else:
+                req_pool_idx = req_pool_indices[seq_idx]
+                per_req_tokens = req_to_token[req_pool_idx, :seq_len_kv]
             per_req_key = k_cache[per_req_tokens].movedim(0, query.dim() - 2)
             per_req_value = v_cache[per_req_tokens].movedim(0, query.dim() - 2)
-
             per_req_out = (
                 scaled_dot_product_attention(
                     per_req_query.unsqueeze(0),
@@ -187,15 +205,22 @@ class TorchNativeAttnBackend(AttentionBackend):
         forward_batch: ForwardBatch,
         save_kv_cache=True,
     ):
+        kv_indices = self.update_kv_indices_encode(forward_batch, layer.is_cross_attention)
         if layer.qk_head_dim != layer.v_head_dim:
             o = q.new_empty((q.shape[0], layer.tp_q_head_num * layer.v_head_dim))
         else:
             o = torch.empty_like(q)
-
+        cache_loc = (
+            forward_batch.out_cache_loc
+            if not layer.is_cross_attention
+            else forward_batch.encoder_out_cache_loc
+        )
         if save_kv_cache:
-            forward_batch.token_to_kv_pool.set_kv_buffer(
-                layer, forward_batch.out_cache_loc, k, v
-            )
+            if k is not None:
+                assert v is not None
+                forward_batch.token_to_kv_pool.set_kv_buffer(
+                    layer, cache_loc, k, v
+                )
 
         use_gqa = layer.tp_q_head_num != layer.tp_k_head_num
 
@@ -207,7 +232,7 @@ class TorchNativeAttnBackend(AttentionBackend):
             o_,
             forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id),
             forward_batch.token_to_kv_pool.get_value_buffer(layer.layer_id),
-            forward_batch.req_to_token_pool.req_to_token,
+            self.req_to_token,
             forward_batch.req_pool_indices,
             forward_batch.seq_lens,
             forward_batch.extend_prefix_lens,
@@ -215,8 +240,107 @@ class TorchNativeAttnBackend(AttentionBackend):
             scaling=layer.scaling,
             enable_gqa=use_gqa,
             causal=not layer.is_cross_attention,
+            kv_indices=kv_indices if layer.is_cross_attention else None,
         )
+        o = o.view(-1, layer.tp_q_head_num*layer.v_head_dim)
         return o
+
+
+    def update_kv_indices_encode(
+        self,
+        forward_batch: ForwardBatch,
+        is_cross_attn=False,
+    ):
+        req_pool_indices = forward_batch.req_pool_indices
+        seq_lens = forward_batch.seq_lens
+        seq_lens_sum = forward_batch.seq_lens_sum
+        encoder_lens = forward_batch.encoder_lens
+        spec_info = forward_batch.spec_info
+        kv_indptr = self.kv_indptr[0]
+        req_to_token = forward_batch.req_to_token_pool.req_to_token
+        if is_cross_attn:
+            paged_kernel_lens = encoder_lens
+            kv_start_idx = torch.zeros_like(encoder_lens)
+            paged_kernel_lens_sum = paged_kernel_lens.sum().item()
+        else:
+            paged_kernel_lens = seq_lens
+            kv_start_idx = encoder_lens
+            paged_kernel_lens_sum = seq_lens_sum
+        bs = len(seq_lens)
+        if spec_info is None:
+            assert len(seq_lens) == len(req_pool_indices)
+            kv_indptr[1 : bs + 1] = torch.cumsum(paged_kernel_lens, dim=0)
+            kv_indptr = kv_indptr[: bs + 1]
+            kv_indices = torch.empty(
+                paged_kernel_lens_sum + 256,
+                dtype=torch.int32,
+                device=req_pool_indices.device,
+            )
+            create_torch_native_kv_indices(
+                req_to_token,
+                req_pool_indices,
+                paged_kernel_lens,
+                kv_indptr,
+                kv_indices,
+                kv_start_idx,
+            )
+        else:
+            assert isinstance(spec_info, EagleDraftInput) or isinstance(
+                spec_info, EagleVerifyInput
+            )
+            kv_indices, kv_indptr, _, _ = (
+                spec_info.generate_attn_arg_prefill(
+                    req_pool_indices,
+                    paged_kernel_lens,
+                    paged_kernel_lens_sum,
+                    req_to_token,
+                )
+            )
+        for i in range(bs):
+            req_pool_index = req_pool_indices[i]
+            self.req_to_token[req_pool_index, :kv_indptr[-1]] = kv_indices[:kv_indptr[-1]]
+        return kv_indices[:kv_indptr[-1]]
+
+
+    def update_kv_indices_decode(
+        self,
+        forward_batch: ForwardBatch,
+        is_cross_attn=False,
+    ):
+        req_pool_indices = forward_batch.req_pool_indices
+        seq_lens = forward_batch.seq_lens
+        seq_lens_sum = forward_batch.seq_lens_sum
+        encoder_lens = forward_batch.encoder_lens
+        spec_info = forward_batch.spec_info
+        kv_indptr = self.kv_indptr[1]
+        req_to_token = forward_batch.req_to_token_pool.req_to_token
+        if is_cross_attn:
+            paged_kernel_lens = encoder_lens
+            kv_start_idx = torch.zeros_like(encoder_lens)
+            seq_lens_sum = encoder_lens.sum().item()
+        else:
+            paged_kernel_lens = seq_lens
+            kv_start_idx = encoder_lens
+        if spec_info is None:
+            bs = len(req_pool_indices)
+            kv_indptr[1 : bs + 1] = torch.cumsum(paged_kernel_lens, dim=0)
+            kv_indptr = kv_indptr[: bs + 1]
+            kv_indices = torch.empty(seq_lens_sum, dtype=torch.int32)
+            create_torch_native_kv_indices(
+                req_to_token,
+                req_pool_indices,
+                paged_kernel_lens,
+                kv_indptr,
+                kv_indices,
+                kv_start_idx,
+            )
+        else:
+            kv_indptr, kv_indices = spec_info.kv_indptr, spec_info.kv_indices
+            bs = kv_indptr.shape[0] - 1
+        for i in range(bs):
+            req_pool_index = req_pool_indices[i].item()
+            self.req_to_token[req_pool_index, :kv_indptr[-1]] = kv_indices[:kv_indptr[-1]]
+        return kv_indices[:kv_indptr[-1]]
 
     def forward_decode(
         self,
@@ -227,19 +351,24 @@ class TorchNativeAttnBackend(AttentionBackend):
         forward_batch: ForwardBatch,
         save_kv_cache=True,
     ):
-        # During torch.compile, there is a bug in rotary_emb that causes the
-        # output value to have a 3D tensor shape. This reshapes the output correctly.
+        kv_indices = self.update_kv_indices_decode(forward_batch, layer.is_cross_attention)
         q = q.reshape(-1, layer.tp_q_head_num * layer.qk_head_dim)
 
         if layer.qk_head_dim != layer.v_head_dim:
             o = q.new_empty((q.shape[0], layer.tp_q_head_num * layer.v_head_dim))
         else:
             o = torch.empty_like(q)
-
+        cache_loc = (
+            forward_batch.out_cache_loc
+            if not layer.is_cross_attention
+            else forward_batch.encoder_out_cache_loc
+        )
         if save_kv_cache:
-            forward_batch.token_to_kv_pool.set_kv_buffer(
-                layer, forward_batch.out_cache_loc, k, v
-            )
+            if k is not None:
+                assert v is not None
+                forward_batch.token_to_kv_pool.set_kv_buffer(
+                    layer, cache_loc, k, v
+                )
 
         use_gqa = layer.tp_q_head_num != layer.tp_k_head_num
 
@@ -251,12 +380,13 @@ class TorchNativeAttnBackend(AttentionBackend):
             o_,
             forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id),
             forward_batch.token_to_kv_pool.get_value_buffer(layer.layer_id),
-            forward_batch.req_to_token_pool.req_to_token,
+            self.req_to_token,
             forward_batch.req_pool_indices,
             forward_batch.seq_lens,
             scaling=layer.scaling,
             enable_gqa=use_gqa,
             causal=False,
+            kv_indices=kv_indices if layer.is_cross_attention else None,
         )
 
         return o
--- python/sglang/srt/layers/linear.py
+++ python/sglang/srt/layers/linear.py
@@ -391,7 +391,7 @@ class ColumnParallelLinear(LinearBase):
         )
         if bias:
             self.bias = Parameter(
-                torch.empty(self.output_size_per_partition, dtype=params_dtype)
+                torch.zeros(self.output_size_per_partition, dtype=params_dtype)
             )
             set_weight_attrs(
                 self.bias,
@@ -1271,7 +1271,7 @@ class RowParallelLinear(LinearBase):
             )
 
         if bias:
-            self.bias = Parameter(torch.empty(self.output_size, dtype=params_dtype))
+            self.bias = Parameter(torch.zeros(self.output_size, dtype=params_dtype))
             set_weight_attrs(
                 self.bias,
                 {
